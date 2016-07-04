f9b8a1facdedc3e3931106ce511c4f3312204154
Ben <Ben@BEN-PC>
2016-07-04 17:22:32 +0800

    手机版预定

--- OA_Game.Web/Scripts/js/Mobile.js
+++ OA_Game.Web/Scripts/js/Mobile.js
@@ -1,4 +1,44 @@
-﻿$(function () {
+﻿var Mobile = {
+    viewModel: {
+        TotalCount: ko.observable(0),
+        RequiredModel: {
+            Phone: ko.observable(),
+            Email: ko.observable()
+        }
+    }
+};
+Mobile.viewModel.GetCount = function () {
+    $.get("/api/Required/", function (result) {
+        Mobile.viewModel.TotalCount(result.TotalCount);
+    });
+}
+Mobile.viewModel.Schedule = function (data, event) {
+    
+    var dom = $(event.target);
+    dom.fadeOut(function () {
+        $($("#book").prev()).fadeIn();
+        $("#book").fadeIn();
+
+    });
+}
+Mobile.viewModel.SubMit = function () {
+    var model = ko.toJS(Mobile.viewModel.RequiredModel);
+    if (model.Phone == null || model.Email == null) {
+        alert("请填写完整");
+    } else {
+        $.post("/api/Required", model, function (result) {
+            if (result.Error) {
+                alert(result.Message);
+            } else {
+                Mobile.viewModel.GetCount();
+                $("#book").hide();
+                $("#TotalCount").show(function () { alert("预定成功"); });
+                
+            }
+        });
+    }
+};
+$(function () {
     var swiper = new Swiper('.swiper-container', {
         pagination: '.swiper-pagination',
         slidesPerView: 2,
@@ -7,5 +47,6 @@
         autoplay: 2500,
         autoplayDisableOnInteraction: false
     });
-
+    ko.applyBindings(Mobile);
+    Mobile.viewModel.GetCount();
 })
